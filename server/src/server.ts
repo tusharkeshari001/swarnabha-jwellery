b82fb4a127fa398de27f87664fe8caebdec48fd2
Tushar.k <Tushar.k@kagamierp.com>
2023-07-07 13:09:13 +0530

    #0001: adding changes in server side application

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,24 @@
 import http from 'http';
 import app from './app';
+import * as sockjs from 'sockjs';
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
 const PORT = 3005;
 const server = http.createServer(app);
+
+
+const echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js' });
+echo.on('connection', function(conn:any) {
+  conn.on('data', function(message:any) {
+    conn.write(message);
+    console.log(message);
+  });
+  conn.on('close', function() {
+    console.log('connection is closed...')
+  });
+});
+
+echo.installHandlers(server, {prefix:'/echo'});
 
 server.listen(PORT, ()=>{
   console.log(`Server running on port ${PORT}`)
